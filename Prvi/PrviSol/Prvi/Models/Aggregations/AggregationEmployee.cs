77b26276ea8f3ebe8881c040f44e3f314da56ca1
niksi99 <104139232+niksi99@users.noreply.github.com>
2023-05-12 18:49:25 +0200

    Agregacije dalje1

--- Prvi/PrviSol/Prvi/Models/Aggregations/AggregationEmployee.cs
+++ Prvi/PrviSol/Prvi/Models/Aggregations/AggregationEmployee.cs
@@ -1,10 +1,13 @@
 ﻿using Prvi.Models.Entities;
+using System.Text.Json.Serialization;
 
 namespace Prvi.Models.Aggregations
 {
     public class AggregationEmployee
     {
+        [JsonPropertyName("plata")]
         public int intAgg { get; set; }
+        [JsonPropertyName("zaposleni")]
         public Employee? employeeAgg { get; set; }
     }
 }
